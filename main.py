f60fd4d7cc6e1251544ba6dddcfd91769cd5f2fe
oodbash <oodbash@outlook.com>
2021-12-29 23:52:04 +0100

    main() added

---
+++ main.py
@@ -0,0 +1,29 @@
+import art
+import game_data
+import random
+
+def main():
+
+    print(art.logo)
+
+    result = 0
+    eog = False
+    
+    while eog == False:
+        first_item = None
+        second_item = None
+        while(first_item == second_item):
+            first_item = random.randint(0,(len(game_data.data)-1))
+            second_item = random.randint(0,(len(game_data.data)-1))
+
+        print(f"Compare A: {game_data.data[first_item]['name']}, a {game_data.data[first_item]['description']}, from {game_data.data[first_item]['country']}, and..")
+        print(f"..against B: {game_data.data[second_item]['name']}, a {game_data.data[second_item]['description']}, from {game_data.data[second_item]['country']}.\n")
+        answer = (str(input("Who has more followers? 'A' or 'B': "))).lower()
+        if game_data.data[first_item]['follower_count'] > game_data.data[second_item]['follower_count'] and answer == "a":
+            result += 1
+            print(f"You are right! Current score: {result}\n")
+        else:
+            print(f"\nSorry, that's wrong. Final score: {result}")
+            eog = True
+
+main()
